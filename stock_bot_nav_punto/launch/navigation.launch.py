c5831f970cbcda87133d1fb529d71d0f82985473
ddcc5305 <107847014+ddcc5305@users.noreply.github.com>
2026-03-26 18:11:48 +0100

    ya se acerca mejor a las coordenadas

--- stock_bot_nav_punto/launch/navigation.launch.py
+++ stock_bot_nav_punto/launch/navigation.launch.py
@@ -3,21 +3,22 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-
         Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
+            # Usamos los temas EXACTOS de tu lista gz topic -l
             arguments=[
-                '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock]',
-                '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry]',
-                '/cmd_vel@geometry_msgs/msg/TwistStamped[gz.msgs.Twist]'
+                '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+                '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+                '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist'
             ],
+            parameters=[{'use_sim_time': True}],
             output='screen'
         ),
-
         Node(
             package='stock_bot_nav_punto',
             executable='navigator',
-            output='screen',
+            parameters=[{'use_sim_time': True}],
+            output='screen'
         )
     ])
