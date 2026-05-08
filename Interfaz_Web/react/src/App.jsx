46005bc18f307fdd3e99d78d3de1de1423ab443a
adrianjafe <145451036+adrianjafe@users.noreply.github.com>
2026-05-08 11:00:54 +0200

    release03 - React en proyecto
    No implementado

---
+++ Interfaz_Web/react/src/App.jsx
@@ -0,0 +1,9 @@
+import PanelHeader from "./PanelHeader"
+
+function App() {
+  return(
+    <PanelHeader/>
+  );
+}
+
+export default App
---
+++ Interfaz_Web/react/src/PanelHeader.jsx
@@ -0,0 +1,15 @@
+function PanelHeader(){
+
+    return(
+        <header>
+            <div className="right-head head-div">
+                <h1>StockBot</h1>
+            </div>
+            <div className="left-head head-div">
+                <h2>Usuario</h2>
+            </div>
+        </header>
+    );
+}
+
+export default PanelHeader
