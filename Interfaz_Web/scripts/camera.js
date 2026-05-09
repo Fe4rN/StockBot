5e754ec25b752c5ed3d23e15a7e2ddc3fa95b9d0
adrianjafe <145451036+adrianjafe@users.noreply.github.com>
2026-05-09 22:10:27 +0200

    release03 - cambio rapido camara

--- Interfaz_Web/scripts/camera.js
+++ Interfaz_Web/scripts/camera.js
@@ -5,6 +5,14 @@
 
 //Función básica para la camara
 function updateCameraFeed(){
+
+    const tab_camara=document.getElementById("tab-camara");
+
+    if(!tab_camara.classList.contains("active")){
+        console.log("retirada");
+        return;
+    }
+    console.log("entrada");
     const img = document.getElementById("cameraFeed");
     const timestamp = new Date().getTime();
     img.src = `http://localhost:8080/stream?topic=/camera/image_raw&t=${timestamp}`;
